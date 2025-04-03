b60bfad306f56c734cd5ba460b23769bf7e9f04f
Fikre-M <fikreddu@gmail.com>
2025-04-03 18:28:34 -0400

    add script

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -3,10 +3,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "evangadi-admin",
+  // host: "localhost",
+  // user: "evangadi-admin",
+  // password: process.env.PASSWORD,
+  // database: process.env.DATABASE,
+  // port: 3307,
+  host: "82.197.82.82",
+  user: "u810119600_evangadiadmin",
   password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  database: u810119600_evangadpractic,
   port: 3307,
 });
 
